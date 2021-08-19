381d5aa2df57d06899c270fd608844e9adfe8636
alfonsohdez08 <alfonsohdez08>
2021-08-19 11:23:13 -0400

    Add perks as list

--- api/CodWeaponsRandomizer/COD/MW/Data/Loadout.cs
+++ api/CodWeaponsRandomizer/COD/MW/Data/Loadout.cs
@@ -4,9 +4,7 @@ public class Loadout
 {
     public WeaponBuild PrimaryWeapon { get; set; }
     public WeaponBuild SecondaryWeapon { get; set; }
-    public string Perk1 { get; set; }
-    public string Perk2 { get; set; }
-    public string Perk3 { get; set; }
+    public List<Perk> Perks { get; set; }
     public string Tactical { get; set; }
     public string Lethal { get; set; }
 }
---
+++ api/CodWeaponsRandomizer/COD/MW/Data/Perk.cs
@@ -0,0 +1,6 @@
+﻿
+namespace CodWeaponsRandomizer.COD.MW.Data;
+public class Perk: GameItem
+{
+    public PerkSlot Slot { get; set; }
+}
