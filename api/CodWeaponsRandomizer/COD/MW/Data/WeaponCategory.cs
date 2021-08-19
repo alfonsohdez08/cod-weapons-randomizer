4e23d33f25e49f0dbbccc1b7f1ef1b056e243a1a
alfonsohdez08 <alfonsohdez08>
2021-08-19 11:11:58 -0400

    Add primary/secondary usage for a weapon category

--- api/CodWeaponsRandomizer/COD/MW/Data/WeaponCategory.cs
+++ api/CodWeaponsRandomizer/COD/MW/Data/WeaponCategory.cs
@@ -3,4 +3,5 @@ namespace CodWeaponsRandomizer.COD.MW.Data;
 public class WeaponCategory: GameItem
 {
     public List<Weapon> Weapons { get; set; }
+    public bool IsForPrimaryUsage { get; set; }
 }
