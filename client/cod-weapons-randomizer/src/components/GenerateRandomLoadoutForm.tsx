53753e4451a5071dd9019eb12f08687d7af20540
alfonsohdez08 <alfonsohdez08>
2021-08-22 21:06:28 -0400

    Add generate random loadout form

--- client/cod-weapons-randomizer/src/components/GenerateRandomLoadoutForm.tsx
+++ client/cod-weapons-randomizer/src/components/GenerateRandomLoadoutForm.tsx
@@ -1,11 +1,25 @@
 import LoadoutGenerationHints from "./LoadoutGenerationHints";
 import Button from "./Button";
 
+import { Container, Row, Col } from "react-bootstrap";
+
 const GenerateRandomLoadoutForm = () => (
-  <div>
-    <LoadoutGenerationHints />
-    <Button placeholder="Generate Loadout" type="submit" theme="secondary" />
-  </div>
+  <Container className="px-0">
+    <Row className="row-cols-1 gy-2">
+      <Col>
+        <LoadoutGenerationHints />
+      </Col>
+      <Col>
+        <div className="d-flex justify-content-center">
+          <Button
+            placeholder="Generate Loadout"
+            type="submit"
+            theme="secondary"
+          />
+        </div>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default GenerateRandomLoadoutForm;
