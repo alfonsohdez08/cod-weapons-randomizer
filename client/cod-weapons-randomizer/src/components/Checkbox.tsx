3cae088dfa3b44896d758e2133221b43c52d3694
alfonsohdez08 <alfonsohdez08>
2021-08-22 21:56:06 -0400

    Add design for bigger screen (loadout hints)

--- client/cod-weapons-randomizer/src/components/Checkbox.tsx
+++ client/cod-weapons-randomizer/src/components/Checkbox.tsx
@@ -6,7 +6,7 @@ const Checkbox = ({ placeholder, value }: InputProps) => {
   const checkBoxValue: string | number =
     typeof value === "boolean" ? Number(value) : value;
 
-  return <Form.Check label={placeholder} value={checkBoxValue} />;
+  return <Form.Check label={placeholder} value={checkBoxValue} className="d-inline-block" />;
 };
 
 export default Checkbox;
--- client/cod-weapons-randomizer/src/components/LoadoutGenerationHints.tsx
+++ client/cod-weapons-randomizer/src/components/LoadoutGenerationHints.tsx
@@ -1,10 +1,29 @@
 import Checkbox from "./Checkbox";
 
-const LoadoutGenerationHints = () => (
-  <div className="border border-2 border-secondary rounded rounded-2 p-2">
-    <Checkbox placeholder="Use all attachment slots" value={false} />
-    <Checkbox placeholder="Use Overkill perk" value={false} />
-  </div>
-);
+const LoadoutGenerationHints = () => {
+  const checkBoxes: JSX.Element[] = [
+    <Checkbox placeholder="Use all attachment slots" value={false} />,
+    <Checkbox placeholder="Use Overkill perk" value={false} />,
+  ];
+
+  return (
+    <>
+      <div className="d-sm-none border border-2 border-secondary rounded rounded-2 p-2">
+        {checkBoxes.map((ch, idx) => (
+          <div key={idx} className="d-block">
+            {ch}
+          </div>
+        ))}
+      </div>
+      <div className="d-none d-sm-flex justify-content-center">
+        {checkBoxes.map((ch, idx) => (
+          <span key={idx} className={idx < checkBoxes.length - 1 ? "me-3" : ""}>
+            {ch}
+          </span>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default LoadoutGenerationHints;
