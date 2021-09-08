88297c438bd9240c4d1652e72d29ebf1c2d860ef
alfonsohdez08 <alfonsohdez08>
2021-09-07 21:39:48 -0400

    move cod dbs to api project folder

--- server/src/CodWeaponsRandomizer/Program.cs
+++ server/src/CodWeaponsRandomizer/Program.cs
@@ -6,8 +6,8 @@ using CodWeaponsRandomizer.Core.COD.Wz;
 using CodWeaponsRandomizer.Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 
-const string MwDbFolderPath = @"..\..\dbs\mw";
-const string CwDbFolderPath = @"..\..\dbs\cw";
+const string MwDbFolderPath = @".\dbs\mw";
+const string CwDbFolderPath = @".\dbs\cw";
 
 const string CorsPolicyName = "RND_COD_LDT";
 
