c950678a0a38c14b86bc688a343f718c15f8bdc1
alfonsohdez08 <alfonsohdez08>
2021-09-10 21:53:40 -0400

    add mobile layout for loadout

--- client/cod-weapons-randomizer/src/components/LoadoutContainer.tsx
+++ client/cod-weapons-randomizer/src/components/LoadoutContainer.tsx
@@ -1,20 +1,76 @@
+import { Col, Container, Row } from "react-bootstrap";
 import { Loadout } from "../api/models";
 
 import WeaponCard from "./WeaponCard";
 
 const LoadoutContainer = ({ loadout }: { loadout: Loadout }) => {
-  const cards: JSX.Element[] = [<WeaponCard weapon={loadout.primaryWeapon} />];
-
   return (
-    <>
-      <div className="d-md-none">{cards}</div>
-      <div className="d-none d-md-block">
-        {cards.map((c) => (
-          <div className="w-50">{c}</div>
-        ))}
-      </div>
-    </>
+    <Container>
+      <Row className="row-cols-1 row-cols-xl-2 gy-2 gy-xl-0 gx-xl-4">
+        <Col>
+          <Row className="row-cols-1">
+            <Col>
+              <LoadoutSectionHeading placeholder="Weapons" />
+            </Col>
+            <Col>
+              <Row className="row-cols-1 row-cols-md-2 gy-2 gy-md-0 gx-md-3">
+                <Col>
+                  <WeaponCard weapon={loadout.primaryWeapon} />
+                </Col>
+                <Col>
+                  <WeaponCard weapon={loadout.secondaryWeapon} />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+        <Col>
+          <Row className="row-cols-1">
+            <Col>
+              <LoadoutSectionHeading placeholder="Perks" />
+            </Col>
+            <Col>
+              <List
+                items={loadout.perks.map((p) => ({ id: p.id, label: p.name }))}
+              />
+            </Col>
+          </Row>
+        </Col>
+        <Col>
+          <Row className="row-cols-1">
+            <Col>
+              <LoadoutSectionHeading placeholder="Tactical" />
+            </Col>
+            <Col>
+              <span>{loadout.tactical.name}</span>
+            </Col>
+          </Row>
+        </Col>
+        <Col>
+          <Row className="row-cols-1">
+            <Col>
+              <LoadoutSectionHeading placeholder="Lethal" />
+            </Col>
+            <Col>
+              <span>{loadout.lethal.name}</span>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 };
+
+const LoadoutSectionHeading = ({ placeholder }: { placeholder: string }) => (
+  <h3 className="d-inline-block">{placeholder}</h3>
+);
+
+const List = ({ items }: { items: { id: number; label: string }[] }) => (
+  <ul className="list-unstyled">
+    {items.map((i) => (
+      <li key={`${i.id}_${i.label}`}>{i.label}</li>
+    ))}
+  </ul>
+);
 
 export default LoadoutContainer;
