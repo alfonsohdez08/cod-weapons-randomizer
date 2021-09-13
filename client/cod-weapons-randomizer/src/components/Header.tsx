c2d2929d5585b2c9fc4fff1b4f77bcec64ab4861
alfonsohdez08 <alfonsohdez08>
2021-09-13 11:27:23 -0400

    Add GitHub logo to home page (it is responsive btw)

--- client/cod-weapons-randomizer/src/components/Header.tsx
+++ client/cod-weapons-randomizer/src/components/Header.tsx
@@ -5,16 +5,17 @@ const Header = () => (
     <div className="d-flex flex-row-reverse">
       <div className="p-2">
         <a href={gitHubRepoUrl}>
-          <img
-            src="/octocat_white_and_black.png"
-            alt="GitHub logo"
-            className="github-logo"
-          />
+          <GitHubImage className="github-logo d-sm-none" />
+          <GitHubImage className="github-logo-sm d-none d-sm-inline-block" />
         </a>
       </div>
     </div>
     <h2 className="text-center display-2 fw-bold">COD Loadout Randomizer</h2>
   </div>
+);
+
+const GitHubImage = ({ className }: { className: string }) => (
+  <img src="/octocat.png" alt="GitHub logo" className={className} />
 );
 
 export default Header;
