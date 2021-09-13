18731a1a2cd39417618be168fd5877e8bda8000e
alfonsohdez08 <alfonsohdez08>
2021-09-13 11:30:53 -0400

    Decrease app heading font size and increase padding

--- client/cod-weapons-randomizer/src/components/Header.tsx
+++ client/cod-weapons-randomizer/src/components/Header.tsx
@@ -3,14 +3,14 @@ const gitHubRepoUrl = "https://github.com/alfonsohdez08/cod-weapons-randomizer";
 const Header = () => (
   <div className="d-flex flex-column">
     <div className="d-flex flex-row-reverse">
-      <div className="p-2">
+      <div className="p-2 p-sm-3">
         <a href={gitHubRepoUrl}>
           <GitHubImage className="github-logo d-sm-none" />
           <GitHubImage className="github-logo-sm d-none d-sm-inline-block" />
         </a>
       </div>
     </div>
-    <h2 className="text-center display-2 fw-bold">COD Loadout Randomizer</h2>
+    <h3 className="text-center display-3 fw-bold">COD Loadout Randomizer</h3>
   </div>
 );
 
