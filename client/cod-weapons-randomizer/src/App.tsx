e23e33a243e61cfe1498990a7c8be328ae64e845
alfonsohdez08 <alfonsohdez08>
2021-09-15 23:20:13 -0400

    Body spans rest of page

--- client/cod-weapons-randomizer/src/App.tsx
+++ client/cod-weapons-randomizer/src/App.tsx
@@ -13,17 +13,19 @@ const App = () => {
   const [loadout, setLoadout] = React.useState<Loadout | undefined>();
 
   return (
-    <div className="text-white">
+    <div className="text-white d-flex flex-column">
       <Header />
-      <div className="w-75 mx-auto">
-        <GenerateRandomLoadoutForm onGenerateLoadout={setLoadout} />
-        {loadout ? (
-          <div className="mt-3 bg-dark border-2 rounded-3 shadow p-2 p-md-4">
-            <LoadoutContainer loadout={loadout} />
-          </div>
-        ) : (
-          <></>
-        )}
+      <div className="flex-grow-1 d-flex flex-column">
+        <div className="w-75 mx-auto flex-grow-1">
+          <GenerateRandomLoadoutForm onGenerateLoadout={setLoadout} />
+          {loadout ? (
+            <div className="mt-3 bg-dark border-2 rounded-3 shadow p-2 p-md-4">
+              <LoadoutContainer loadout={loadout} />
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     </div>
   );
